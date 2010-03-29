c6a992df8c8ee36bdd0a2a2606fb00cd3cd53907
Daniel Ruoso <daniel@ruoso.com>
2010-03-29 09:53:34 -0300

    Documenting the README. Add the copyright notice. Fix the samples. Clean unnecessary files.

--- xsdForm.js
+++ xsdForm.js
@@ -1,3 +1,22 @@
+/*
+# Copyright 2010 - Prefeitura Municipal de Fortaleza
+#
+# Este arquivo é parte do programa xsdform-js
+#
+# O xsdform-js é um software livre; você pode redistribui-lo e/ou
+# modifica-lo dentro dos termos da Licença Pública Geral GNU como
+# publicada pela Fundação do Software Livre (FSF); na versão 2 da
+# Licença.
+#
+# Este programa é distribuido na esperança que possa ser util, mas SEM
+# NENHUMA GARANTIA; sem uma garantia implicita de ADEQUAÇÂO a qualquer
+# MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU
+# para maiores detalhes.
+#
+# Você deve ter recebido uma cópia da Licença Pública Geral GNU, sob o
+# título "LICENCA.txt", junto com este programa, se não, escreva para a
+# Fundação do Software Livre(FSF) Inc., 51 Franklin St, Fifth Floor,
+*/
 
 function createInput(type, name, id) {
     var newInput = document.createElement('input');
