a76a6e41f4477b513a8780b5adbff06e1ea9ab62
rlafitte.fa7@gmail.com <lafitte@lafitte-desktop.(none)>
2011-04-05 14:44:17 -0300

    compatibilidade com o google chrome

--- xsdForm.js
+++ xsdForm.js
@@ -683,7 +683,7 @@ function generateFormIteration(xsdFile,containerId,Iteration) {
 
         //carrega o xml
         var xml = xmlLoader(xsdFile);
-        var tagRaiz  = xml.getElementsByTagName('xs:schema')[0];
+        var tagRaiz  = xml.getElementsByTagNameNS('xs:schema')[0];
         var elemRoot = getNodeByTagName(tagRaiz, 'xs:element'); // elemento raiz
         var elHtml = generateFormFromNode(tagRaiz, elemRoot, "xsdform___"+Iteration);
         getById(containerId).appendChild( elHtml );
